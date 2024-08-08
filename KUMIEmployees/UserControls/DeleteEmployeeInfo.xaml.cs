c24adb9c543e7e00cc5fd17b575cff8be4b23e78
giolga <giolga.kumi@gmail.com>
2024-08-08 16:58:34 +0400

    Delete UC UI done

--- KUMIEmployees/UserControls/DeleteEmployeeInfo.xaml.cs
+++ KUMIEmployees/UserControls/DeleteEmployeeInfo.xaml.cs
@@ -24,5 +24,40 @@ namespace KUMIEmployees.UserControls
         {
             InitializeComponent();
         }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DeleteBtn.Cursor = Cursors.Hand;
+        }
+
+        private void DeleteBtn_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DeleteBtn.Cursor = Cursors.Arrow;
+        }
+
+        private void MaleRB_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MaleRB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FemaleRB_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FemaleRB_Unchecked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
